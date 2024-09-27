08a8d6643c527be4958b20963a4507efac38eb9a
Platformer10101 <platformer10101@gmail.com>
2024-09-27 14:55:13 -0400

    Add files via upload

---
+++ worst_guessing_game.py
@@ -0,0 +1,10 @@
+number = 4
+
+print(" teh worst number gessing game evar!!!!!!!1!")
+
+guess = int(input("I'M THKING OF A NBR FROM 1-10.  TRY 2 GESS! "))
+
+if guess == number:
+    print(f"LOL!!! U GOT IT!  I CANT BELEIVE U GESSED IT WAS {number}!")
+else:
+    print(f"W00T!  U SUX0R!!!  I PWN J00!!!  IT WAS {number}!")
